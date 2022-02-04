7ae76799c4ef86c4b2ce75dc6129f5bd5baa870a
CesarArellano <raywayday@gmail.com>
2022-02-04 13:31:41 -0600

    Adding new code

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:on_audio_query/on_audio_query.dart';
 
@@ -10,8 +13,11 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final AudioPlayer audioPlayer = AudioPlayer();
   final OnAudioQuery onAudioQuery = OnAudioQuery();
-  List<SongModel> songsFound = [];
+  bool _isLoading = false;
+
+  List<SongModel> songList = [];
 
   @override
   void initState() {
@@ -20,19 +26,38 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   void searchSongs() async {
-    songsFound = await onAudioQuery.querySongs();
-    print(songsFound);
-    for (SongModel song in songsFound) {
-      debugPrint(song.title);
+    setState(() => _isLoading = true);
+    if( ! await onAudioQuery.permissionsStatus() ) {
+      await onAudioQuery.permissionsRequest();
     }
+    songList = await onAudioQuery.querySongs();
+    setState(() => _isLoading = false);
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('HomeScreen'),
-      ),
+    return Scaffold(
+      body: _isLoading
+        ? const Center ( child: CircularProgressIndicator() )
+        : ListView.builder(
+          physics: const BouncingScrollPhysics(),
+          itemCount: songList.length,
+          itemBuilder: ( _, int i ) => ListTile(
+            contentPadding: const EdgeInsets.symmetric( vertical: 10, horizontal: 15),
+            title: Text(songList[i].title),
+            subtitle: Text(songList[i].artist ?? 'No Artist'),
+            onTap: () async {
+              final uri = '${ songList[i].uri }';
+              final file = File(uri);
+              print(file.path);
+
+            },
+            leading: QueryArtworkWidget(
+              id: songList[i].id,
+              type: ArtworkType.AUDIO,
+            ),
+          )
+        )
     );
   }
 }
