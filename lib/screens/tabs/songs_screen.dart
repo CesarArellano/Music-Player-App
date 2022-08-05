605933aca58bfdac3d5221665d79f6c6cacd2291
CesarArellano <raywayday@gmail.com>
2022-08-04 23:17:27 -0500

    Fixed SongsScreen img path

--- lib/screens/tabs/songs_screen.dart
+++ lib/screens/tabs/songs_screen.dart
@@ -39,7 +39,7 @@ class _SongsScreenState extends State<SongsScreen> with AutomaticKeepAliveClient
           itemCount: songList.length,
           itemBuilder: ( _, int i ) {
             final song = songList[i];
-                                  final imageFile = File(MusicActions.getArtworkPath(song.data) ?? '');
+            final imageFile = File(MusicActions.getArtworkPath(song.data) ?? '');
 
             return RippleTile(
               child: ListTile(
